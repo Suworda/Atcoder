737d5d5c634ad5513c996b91ce47ba71e3e9c937
Suworda <su5200su@gmail.com>
2025-10-05 07:38:28 +0800

    Update D - Ulam-Warburton Automaton.cpp

--- abc/abc425/D - Ulam-Warburton Automaton.cpp
+++ abc/abc425/D - Ulam-Warburton Automaton.cpp
@@ -1,78 +1,55 @@
 #include <iostream>
-#include <queue>
-using namespace std;
+#include <vector>
+using namespace  std;
+int h,w;
 int dr[]={1,0,-1,0};
 int dc[]={0,1,0,-1};
-int h,w;
 
-bool ok(int r, int c){
-	return (r>=1 && r<=h && c>=1 && c<=w);
-}
+#define x first
 
-int main() {
-	int ans=0;
-	queue<pair<int,int>> que;
-	cin>>h>>w;
-	bool a[h+5][w+5]={};
-	char c;
-	for(int i=1; i<=h; i++){
-		for(int j=1; j<=w; j++){
-			cin>>c;
-			if(c=='#'){
-//				ans++;
-				a[i][j]='#';
-				for(int k=0; k<4; k++){
-					if(ok(i+dr[k] , j+dc[k])) que.push({i+dr[k] , j+dc[k]});
-				}
-			}
-		}
-	}
-	
-	while(!que.empty()){
-		pair<int,int> cur=que.front();
-		int i=cur.first;
-		int j=cur.second;
-		que.pop();
-		if(a[i][j]) continue;
-		int cnt=0;
-		
-		for(int k=0; k<4; k++){
-			if(ok(i+dr[k] , j+dc[k])) cnt+=a[i+dr[k]][j+dc[k]];
-		}
-		
-		if(cnt==1){
-			a[i][j]=true;
-			for(int k=0; k<4; k++){
-				if(ok(i+dr[k] , j+dc[k])) que.push({i+dr[k] , j+dc[k]});
-			}
-		}
-	}
-	
-	for(int i=1; i<=h; i++){
-		for(int j=1; j<=w; j++){
-			int cnt=0;
-			for(int k=0; k<4; k++){
-				if(ok(i+dr[k] , j+dc[k])) cnt+=a[i+dr[k]][j+dc[k]];
-			}
-		
-			if(cnt==1){
-				a[i][j]=true;
-				for(int k=0; k<4; k++){
-					if(ok(i+dr[k] , j+dc[k])) que.push({i+dr[k] , j+dc[k]});
-				}
-			}
-		}
-	}
-	
-	
-	for(int i=1; i<=h; i++){
-		for(int j=1; j<=w; j++){
-			cout<<a[i][j];
-			ans+=a[i][j];
-		}
-		cout<<'\n';
-	}
-	
-	cout<<ans<<'\n';
-	
+int main(){
+    cin>>h>>w;
+    vector<vector<int>> a(h+2,vector<int>(w+2,-1));
+    vector<pair<int,int>> check ,draw;
+    char c;
+    for(int i=1; i<=h; i++){
+        for(int j=1; j<=w; j++){
+            cin>>c;
+            if(c=='#'){
+                draw.push_back({i,j});
+            }else{
+                a[i][j]=0;
+            }
+        }
+    }
+
+    int nr,nc,cnt,ans=0;
+    while(!draw.empty()){
+        for(auto [r,c]:draw){
+            a[r][c]=1;
+            ans++;
+            for(int k=0; k<4; k++){
+                nr=r+dr[k];
+                nc=c+dc[k];
+                if(a[nr][nc]==0) check.push_back({nr,nc});
+            }
+        }
+        draw.clear();
+
+        for(auto [r,c]:check){
+//            cout<<r<<' '<<c<<'\n';
+            cnt=0;
+            for(int k=0; k<4; k++){
+                nr=r+dr[k];
+                nc=c+dc[k];
+                if(a[nr][nc]==1) cnt++;
+            }
+            if(cnt==1) draw.push_back({r,c});
+        }
+
+        check.clear();
+    }
+
+    cout<<ans<<'\n';
+
 }
