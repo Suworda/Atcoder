e6d3b7d5b56cf1711ec203eaa630e34057e91618
Suworda <su5200su@gmail.com>
2025-11-27 23:44:56 +0800

    Create D - ABC Transform.cpp

---
+++ abc/abc242/D - ABC Transform.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+
+char fix(long long c){
+    return char((c-'A')%3 + 'A');
+}
+
+char f(long long t, long long k){
+    if(k==1) return fix(s[0] + t%3);
+    if(t==0) return s[k-1];
+
+    return fix(f(t-1, (k+1)/2) + k%2);
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie();
+
+    cin>>s;
+
+    int q;
+    cin>>q;
+    while(q--){
+        long long t,k;
+        cin>>t>>k;
+        cout<<f(t,k)<<'\n';
+    }
+}
